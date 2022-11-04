ecf336f780c8d808a3271085c67479c6122aed08
Andrii Skokovskyi <askokovskiy@gmail.com>
2022-11-04 20:15:09 +0200

    feat: Add main boilerplate

--- src/App.js
+++ src/App.js
@@ -1,5 +1,23 @@
+import { Button, Container, Stack } from "react-bootstrap";
+
 function App() {
-  return <div>Hello world</div>;
+  return (
+    <Container className="my-4">
+      <Stack direction="horizontal" gap="2" className="mb-4">
+        <h1 className="me-auto">Budgets</h1>
+        <Button variant="primary">Add Budget</Button>
+        <Button variant="outline-primary">Add Expense</Button>
+      </Stack>
+      <div
+        style={{
+          display: "grid",
+          gridTemplateColumns: "repeat(auto-fill, minmax(300px, 1fr))",
+          gap: "1rem",
+          alignItems: "flex-start",
+        }}
+      ></div>
+    </Container>
+  );
 }
 
 export default App;
